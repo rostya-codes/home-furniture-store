bb9fdca3db7a2e6368031ee637ca949ac7a7ddec
rostya <rostyamudrik@proton.me>
2024-08-12 16:06:59 +0300

    Added goods app, connected catalog & product html pages

--- home_furniture_store/urls.py
+++ home_furniture_store/urls.py
@@ -4,5 +4,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main')),
+    path('', include('main.urls', namespace='main')),  # main app
+    path('catalog/', include('goods.urls', namespace='catalog')),  # goods app
 ]
+
+# 2:43:28
