5b04754658d0f27f0e9ced615a2b156f07a6581a
rostya <rostyamudrik@proton.me>
2024-08-14 13:29:24 +0300

    <slug:product_slug>/ and <int:product_id>/ converters for product added

--- goods/urls.py
+++ goods/urls.py
@@ -6,5 +6,6 @@ app_name = 'goods'
 
 urlpatterns = [
     path('', views.catalog, name='index'),
-    path('product/', views.product, name='product')
+    path('product/<int:product_id>/', views.product, name='product'),
+    path('product/<slug:product_slug>/', views.product, name='product'),
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,5 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render
 
 from goods.models import Product
@@ -14,5 +16,15 @@ def catalog(request):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug=None, product_id=None):
+
+    if product_id:
+        product = Product.objects.get(id=product_id)
+    else:
+        product = Product.objects.get(slug=product_slug)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'goods/product.html', context)
