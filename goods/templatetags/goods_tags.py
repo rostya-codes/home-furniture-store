236d385569047b01ff0fba36a3dd2c58f2be0442
rostya <rostyamudrik@proton.me>
2024-09-04 14:37:31 +0300

    The product filter on the site page and urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.http import urlencode
 
 from goods.models import Category
 
@@ -10,3 +11,10 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Category.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
--- goods/views.py
+++ goods/views.py
@@ -7,11 +7,18 @@ from goods.models import Product
 def catalog(request, category_slug):
 
     page = request.GET.get('page', 1)  # Получить из ссылки номер страницы по ключу, дефолт значение - 1
+    on_sale = request.GET.get('on_sale', None)  # Получить из ссылки номер страницы по ключу, дефолт значение - 1
+    order_by = request.GET.get('order_by', None)  # Получить из ссылки номер страницы по ключу, дефолт значение - 1
 
     if category_slug == 'all':
         goods = Product.objects.all()
     else:
         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
