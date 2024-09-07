dcfd9f8d28834881df3344b9f28c7ff618785d7a
rostya <rostyamudrik@proton.me>
2024-09-07 11:44:36 +0300

    Registration form and controller are ready

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,16 @@
 from cProfile import label
 
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
 
     username = forms.CharField()
     password = forms.CharField()
@@ -19,6 +23,74 @@ class UserLoginForm(AuthenticationForm):
     #                                                              'placeholder': 'Введите ваш пароль'}))
 
 
+class UserRegistrationForm(UserCreationForm):
+
     class Meta:
-            model = User
-            fields = ['username', 'password']
+        model = User
+
+        # Имена как в базе данных
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        )
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+
+    # first_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваше имя",
+    #         }
+    #     )
+    # )
+    # last_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите вашу фамилию",
+    #         }
+    #     )
+    # )
+    # username = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваше имя пользователя",
+    #         }
+    #     )
+    # )
+    # email = forms.CharField(
+    #     widget=forms.EmailInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваш email *youremail@example.com",
+    #         }
+    #     )
+    # )
+    # password1 = forms.CharField(
+    #     widget=forms.PasswordInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваш пароль",
+    #         }
+    #     )
+    # )
+    # password2 = forms.CharField(
+    #     widget=forms.PasswordInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Подтвердите ваш пароль",
+    #         }
+    #     )
+    # )
--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -27,18 +27,30 @@ def login(request):
 
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
     context = {
-        'title': 'Home - Регистрация'
+        'title': 'Home - Регистрация',
+        'form': form,
     }
     return render(request, 'users/registration.html', context)
 
 
 def profile(request):
     context = {
-        'title': 'Home - Кабинет'
+        'title': 'Home - Кабинет',
     }
     return render(request, 'users/profile.html', context)
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
