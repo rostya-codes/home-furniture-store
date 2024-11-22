e3d65a22e5ef85b8050e7cf9cb31cd46410a8630
rostya <rostyamudrik@proton.me>
2024-11-23 01:42:12 +0200

    orders models and forms added

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users/images', blank=True, null=True, verbose_name='Аватар')
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.username
